610771d86e5c2124cd976d256c2e4b0b243e8a90
Antonio Santos <antoniosantos@creoate.com>
2024-04-03 14:56:10 +0200

    Extended witn Content Collections and View Transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrotutorialsantos.netlify.app/"
+  site: "https://astrotutorialsantos.netlify.app/",
+  integrations: [preact()]
 });
